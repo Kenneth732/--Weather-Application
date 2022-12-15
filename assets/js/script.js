53270a8949d741bd6c7e4514c9aeb7d1639a7a99
KENNETH <81636030+Kenneth732@users.noreply.github.com>
2022-12-15 09:57:46 +0300

    js selectors
    add selectors to js file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// js selectors
+var inputval = document.querySelector('#cityinput');
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput');
+var descrip = document.querySelector('#description');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+apik = "3045dd712ffe6e702e3245525ac7fa38"
