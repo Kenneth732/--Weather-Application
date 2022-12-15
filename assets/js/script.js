03ac456bbfa8e90a3d7636134a38578d4936c588
KENNETH <81636030+Kenneth732@users.noreply.github.com>
2022-12-15 10:18:51 +0300

    let add event listener for fetching data
    0ab4ff7953a537a280cc34664ce825ac

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,10 @@ var descrip = document.querySelector('#description');
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
 apik = "3045dd712ffe6e702e3245525ac7fa38"
+
+
+// 
+function convertion(val){
+    return (val - 273).toFixed(2)
+}
+//Now we have to collect all the information with the help of fetch method
